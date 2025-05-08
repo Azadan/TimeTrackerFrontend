91ecbd5618199787adb5e731687563f945dee34d
Azadan <azadan@live.com>
2025-05-08 23:12:03 +0200

    create category call and getcategories

---
+++ src/Api/Category.ts
@@ -0,0 +1,36 @@
+import type {createCategoryRequest} from "../Types/Category.ts";
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080/api/v1/category';
+
+export const createCategory = async (categoryData: createCategoryRequest) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.post(`${API_URL}/create`, categoryData, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        // @ts-ignore
+        throw new error('Failed to create category');
+    }
+}
+
+export const getCategories = async (userId: number) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get(`${API_URL}/find/${userId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        // @ts-ignore
+        throw new error('Failed to fetch categories');
+    }
+}
