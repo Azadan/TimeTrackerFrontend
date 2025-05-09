a56ba2e0976aee5ae93c7363c4d7a50375e16349
Azadan <azadan@live.com>
2025-05-09 02:03:37 +0200

    check in and out req

---
+++ src/Types/ActivateCategory.ts
@@ -0,0 +1,10 @@
+export type CheckInRequest = {
+    userId: number
+    categoryId: number
+}
+
+export interface CheckinRequest {
+    userId: number
+    categoryId: number
+    entryId: number
+}
